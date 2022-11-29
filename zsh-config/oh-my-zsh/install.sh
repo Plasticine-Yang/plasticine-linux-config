3f1d6384dbe227040535e54e6346ec27335ccc98
plasticine9750 <975036719@qq.com>
2022-11-29 21:59:35 +0800

    feat(zsh-config): update oh-my-zsh configuration

--- zsh-config/oh-my-zsh/install.sh
+++ zsh-config/oh-my-zsh/install.sh
@@ -1,5 +1,3 @@
-# zsh-history-substring-search
-git clone https://ghproxy.com/https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 # zsh-autosuggestions
 git clone https://ghproxy.com/https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # zsh-syntax-highlighting
