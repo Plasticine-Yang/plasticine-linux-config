689aa3f01247f62272151067eefa102c55c5a518
plasticine9750 <975036719@qq.com>
2022-11-30 10:09:12 +0800

    feat(nvim): plugins

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,55 @@
+-- 自动安装 packer.nvim
+-- 插件安装目录
+-- ~/.local/share/nvim/site/pack/packer/
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    vim.notify("installing packer.nvim...")
+
+    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+    vim.cmd [[packadd packer.nvim]]
+
+    vim.notify("packer.nvim was successfully installed.")
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  vim.notify("packer.nvim is not installed!")
+  return
+end
+
+packer.startup({
+  function(use)
+    use 'wbthomason/packer.nvim'
+
+    -- My plugins here
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+      require('packer').sync()
+    end
+  end,
+
+  config = {
+    -- using a floating window for command outputs
+    display = {
+      open_fn = require('packer.util').float
+    }
+  }
+})
+
+-- 保存 plugins.lua 文件自动安装插件
+pcall(vim.cmd, [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
